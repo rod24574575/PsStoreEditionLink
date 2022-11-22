e4366c18a44369131b307e7d24d4fe5e67fde7b3
rod24574575 <rod24574575@hotmail.com>
2022-11-22 22:50:43 +0800

    release v0.2.0

--- PsStoreEditionLink.user.js
+++ PsStoreEditionLink.user.js
@@ -2,7 +2,7 @@
 // @name            PsStoreEditionLink
 // @namespace       PsStoreEditionLink
 // @description     Add hyperlink to the other editions in playstation store
-// @version         0.1.0
+// @version         0.2.0
 // @license         MIT
 // @author          rod24574575
 // @homepageURL     https://github.com/rod24574575/PsStoreEditionLink
